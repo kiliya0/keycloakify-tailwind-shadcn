25ab4300774740ae1bd1257b8df4917833e02470
Kuanysh Iliya <kuanysh.ilya@gmail.com>
2024-11-19 16:37:16 +0500

    fixed version

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -1,5 +1,15 @@
 import { createUseI18n } from "keycloakify/login";
 
-export const { useI18n, ofTypeI18n } = createUseI18n({});
+export const { useI18n, ofTypeI18n } = createUseI18n({
+    ru: {
+        confirmExecutionOfActions: "Выполните следующие действия ",
+        "requiredAction.UPDATE_PASSWORD": "Обновление пароля",
+        proceedWithAction: "&raquo; Нажмите здесь чтобы перейти",
+        resetPasswordMessage: "Необходимо установить новый пароль",
+        expiredActionMessage: "Действие истекло. Пожалуйста, продолжите вход.",
+        expiredActionTokenNoSessionMessage: "Действие истекло.",
+        expiredActionTokenSessionExistsMessage: "Действие истекло. Пожалуйста, начните сначала.",
+    }
+});
 
 export type I18n = typeof ofTypeI18n;
